a7ebfad221e243d0536e4fbaa4ed4027661bd596
Shehab Ihab <shehab.ihab@cyshield.com>
2025-01-13 18:09:25 +0200

    chunking works while respecting newlines

--- main.go
+++ main.go
@@ -2,32 +2,40 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"testing"
 )
 
 const CHUNK_SIZE = 250 * 1024 * 1024
-const FILE_NAME = "fake_log_1024MB.txt"
+const FILE_NAME = "fake_log_5120MB.txt"
 
-func LoadFileChunk(file *os.File, reader *bufio.Reader, buf *[]byte, offset int64) (*[]byte, int64, error) {
-
+func LoadFileChunk(file *os.File, buf []byte, offset int64) ([]byte, int64, error) {
 	_, err := file.Seek(offset, 0)
 	if err != nil {
 		return nil, offset, fmt.Errorf("error seeking to offset %d: %w", offset, err)
 	}
-
-	n, err := reader.Read(*buf)
-	if err != nil {
-		if err.Error() == "EOF" {
-			return nil, offset, err
-		}
+	reader := bufio.NewReaderSize(file, CHUNK_SIZE)
+	n, err := reader.Read(buf)
+	if err != nil && err != io.EOF {
 		return nil, offset, fmt.Errorf("error reading file: %w", err)
 	}
 
-	(*buf) = (*buf)[:n]
+	buf = buf[:n]
 
-	return buf, offset + int64(n), nil
+	if err == io.EOF {
+		return buf, offset + int64(n), err
+	}
+
+	lastNewLine := bytes.LastIndexByte(buf, '\n')
+	if lastNewLine == -1 {
+		panic("chunk size too small to contain even one line, increase chunk size")
+	}
+
+	buf = buf[:lastNewLine]
+	return buf, offset + int64(lastNewLine+1), nil
 }
 
 func main() {
@@ -39,32 +47,28 @@ func main() {
 	}
 	defer file.Close()
 
-	reader := bufio.NewReaderSize(file, CHUNK_SIZE)
-
 	buf := make([]byte, CHUNK_SIZE)
 
-	// newFile, err := os.Create("test.txt")
-	// if err != nil {
-	// 	fmt.Printf("Error opening file: %v\n", err)
-	// 	return
-	// }
-	// defer newFile.Close()
+	newFile, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer newFile.Close()
 
 	var offset int64 = 0
 	for {
-		_, newOffset, err := LoadFileChunk(file, reader, &buf, offset)
+		_, newOffset, err := LoadFileChunk(file, buf, offset)
 		if err != nil {
 			if err.Error() == "EOF" {
-				// End of file reached, stop reading
 				// fmt.Println("End of file reached")
 				break
 			}
-			fmt.Printf("Error loading file chunk: %v\n", err)
+			// fmt.Printf("Error loading file chunk: %v\n", err)
 			return
 		}
 
-		// fmt.Printf("Read chunk of size: %d bytes\n", len(*chunk))
-		// newFile.Write(chunk)
+		// fmt.Printf("Read chunk of size: %d bytes\n", len(chunk))
 
 		offset = newOffset
 	}
