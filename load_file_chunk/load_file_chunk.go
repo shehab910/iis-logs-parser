19379b1e03e9677cfed7bfa64e1b43df2f2b6c24
Shehab Ihab <shehab.ihab@cyshield.com>
2025-01-15 15:55:52 +0200

    Refactor LoadFileChunk error handling and add TODO for dynamic chunk size adjustment

--- load_file_chunk/load_file_chunk.go
+++ load_file_chunk/load_file_chunk.go
@@ -3,11 +3,19 @@ package load_file_chunk
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"testing"
 )
+
+//TODO FIXME: Dynamically adjust chunk size based on the file size for better performance:
+// When processing large files, it’s beneficial to dynamically adjust the chunk size based
+// on the file size to optimize performance. Instead of using a fixed chunk size,
+// you can make the chunk size dynamic, scaling it up for larger files and
+// reducing it for smaller files. This helps avoid excessive memory usage for large files
+// while ensuring efficient reading of smaller files.
 
 const CHUNK_SIZE = 8 //250 * 1024 * 1024
 const FILE_NAME = "test_read.txt"
@@ -22,13 +30,13 @@ func LoadFileChunk(file *os.File, offset int64) ([]byte, int64, error) {
 	reader := bufio.NewReaderSize(file, CHUNK_SIZE)
 
 	n, err := reader.Read(buf)
-	if err != nil && err != io.EOF {
+	if err != nil && errors.Is(err, io.EOF) {
 		return nil, offset, fmt.Errorf("error reading file: %w", err)
 	}
 
 	buf = buf[:n]
 
-	if err == io.EOF {
+	if errors.Is(err, io.EOF) {
 		return buf, offset + int64(n), err
 	}
 
@@ -61,7 +69,7 @@ func main() {
 	for {
 		_, newOffset, err := LoadFileChunk(file, offset)
 		if err != nil {
-			if err.Error() == "EOF" {
+			if errors.Is(err, io.EOF) {
 				// fmt.Println("End of file reached")
 				break
 			}
